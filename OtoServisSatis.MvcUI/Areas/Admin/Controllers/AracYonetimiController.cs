110f5b2f0b450ba00abcddb876b01a3d5ac149b6
mesutilica <mesut.ilica@gmail.com>
2021-09-11 15:07:12 +0300

    11-09-21 Mvc Araç ve Kullanıcı yönetim ekranları yapıldı

---
+++ OtoServisSatis.MvcUI/Areas/Admin/Controllers/AracYonetimiController.cs
@@ -0,0 +1,119 @@
+﻿using OtoServisSatis.BL;
+using OtoServisSatis.Entities;
+using System;
+using System.Web.Mvc;
+
+namespace OtoServisSatis.MvcUI.Areas.Admin.Controllers
+{
+    public class AracYonetimiController : Controller
+    {
+        AracManager manager = new AracManager();
+        MarkaManager markaManager = new MarkaManager();
+        // GET: Admin/AracYonetimi
+        public ActionResult Index()
+        {
+            return View(manager.GetAll());
+        }
+        public ActionResult Create()
+        {
+            ViewBag.MarkaId = new SelectList(markaManager.GetAll(), "Id", "Adi");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Arac arac)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var sonuc = manager.Add(arac);
+                    if (sonuc > 0)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Hata Oluştu! Kayıt Eklenemedi!");
+            }
+            
+            return View();
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Arac arac = manager.Find(id.Value);
+            if (arac == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MarkaId = new SelectList(markaManager.GetAll(), "Id", "Adi", arac.MarkaId);
+            return View(arac);
+        }
+        [HttpPost]
+        public ActionResult Edit(Arac arac)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var sonuc = manager.Update(arac);
+                    if (sonuc > 0)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Hata Oluştu! Kayıt Eklenemedi!");
+            }
+            ViewBag.MarkaId = new SelectList(markaManager.GetAll(), "Id", "Adi", arac.MarkaId);
+            return View(arac);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Arac arac = manager.Find(id.Value);
+            if (arac == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(arac);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int? id, string test)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Arac arac = manager.Find(id.Value);
+            if (arac == null)
+            {
+                return HttpNotFound();
+            }
+            try
+            {
+                manager.Delete(id.Value);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("","Kayıt Silinemedi!");
+            }
+            return View(arac);
+        }
+
+    }
+}
---
+++ OtoServisSatis.MvcUI/Areas/Admin/Controllers/KullaniciYonetimiController.cs
@@ -0,0 +1,110 @@
+﻿using OtoServisSatis.BL;
+using OtoServisSatis.Entities;
+using System;
+using System.Web.Mvc;
+
+namespace OtoServisSatis.MvcUI.Areas.Admin.Controllers
+{
+    public class KullaniciYonetimiController : Controller
+    {
+        KullaniciManager manager = new KullaniciManager();
+        RoleManager roleManager = new RoleManager();
+        // GET: Admin/KullaniciYonetimi
+        public ActionResult Index()
+        {
+            return View(manager.GetAll());
+        }
+
+        
+        // GET: Admin/KullaniciYonetimi/Create
+        public ActionResult Create()
+        {
+            ViewBag.RolId = new SelectList(roleManager.GetAll(), "Id", "Adi");
+            return View();
+        }
+
+        // POST: Admin/KullaniciYonetimi/Create
+        [HttpPost]
+        public ActionResult Create(Kullanici kullanici)
+        {
+            try
+            {
+                kullanici.EklenmeTarihi = DateTime.Now;
+                manager.Add(kullanici);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/KullaniciYonetimi/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Kullanici kullanici = manager.Find(id.Value);
+            if (kullanici == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RolId = new SelectList(roleManager.GetAll(), "Id", "Adi", kullanici.RolId);
+            return View(kullanici);
+        }
+
+        // POST: Admin/KullaniciYonetimi/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Kullanici kullanici)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    manager.Update(kullanici);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/KullaniciYonetimi/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Kullanici kullanici = manager.Find(id.Value);
+            if (kullanici == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(kullanici);            
+        }
+
+        // POST: Admin/KullaniciYonetimi/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                manager.Delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
